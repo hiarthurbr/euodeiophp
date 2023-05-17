a0d50f04de19f96c66b94d63e7783da86437d3f8
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2023-05-17 14:13:59 -0300

    css atv5 ex6 feito

--- atv5/ex6/senha.php
+++ atv5/ex6/senha.php
@@ -74,71 +74,178 @@
       }
     }
   </script>
+  <style>
+    body {
+      font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    }
+
+    .bg {
+      z-index: -999;
+      top:0;
+      left:0;
+      right:0;
+      bottom:0;
+      position: fixed;
+      width:100%;
+      height:100%;
+      padding-top: 4rem;
+      background: #9090f0;
+      overflow:hidden;
+    }
+
+    .center {
+      position: fixed;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    .title {
+      font-size: 4rem;
+      font-weight: 700;
+      color: #fff;
+    }
+
+    .round-input {
+      border-radius: 10px;
+      border: 1px solid #ccc;
+      padding: 10px;
+      margin: 10px; 
+    }
+
+    .pretty-button {
+      border-radius: 10px;
+      border: 1px solid #ccc;
+      padding: 10px;
+      margin: 10px;
+    }
+
+    .pretty-button:hover {
+      background-color: #ccc;
+      cursor: pointer;
+    }
+
+    .box {
+      --tw-backdrop-blur: blur(8px);
+      backdrop-filter: blur(20px);
+      border-radius: 4rem;
+      padding: .1rem;
+      padding-left: 4rem;
+      padding-right: 4rem;
+      background-color: #00000080;
+    }
+    
+    code {
+      font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+        monospace;
+      background-color: lightgray;
+    }
+
+    .betinho-container {
+      height: 90vh;
+      width: 50vw;
+      position: absolute;
+      overflow: hidden;
+      bottom: 0;
+      right: 0;
+      margin-bottom: 0;
+      margin-right: 0;
+      z-index: -1;
+    }
+    .betinho {
+      height: 100%;
+      width: auto;
+      right: 0;
+      position: absolute;
+    }
+    .betinho-title {
+      font-size: 1.5rem;
+      font-weight: 700;
+      color: #fff;
+      position: absolute;
+      bottom: 40vh;
+      right: 0;
+      margin-bottom: 0;
+      margin-right: 0;
+      z-index: 1;
+      transform: translate(-50%);
+    }
+  </style>
 </head>
 
 <body>
-  <h1>Gerador de senha</h1>
-  <?php
-  if (isset($_POST['tamanho']) && isset($_POST['primeiro'])) {
-    $tamanho = (int) $_POST['tamanho'];
-    $first = $_POST['primeiro'];
-    $seguintes = isset($_POST['seguintes']) ? $_POST['seguintes'] : [];
+  <div class="center">
+    <h1 class="title">Gerador de senha</h1>
+    <?php
+    if (isset($_POST['tamanho']) && isset($_POST['primeiro'])) {
+      $tamanho = (int) $_POST['tamanho'];
+      $first = $_POST['primeiro'];
+      $seguintes = isset($_POST['seguintes']) ? $_POST['seguintes'] : [];
 
-    $numeros = range(0, 9);
-    $maiusculas = range('A', 'Z');
-    $minusculas = range('a', 'z');
-    $simbolos = str_split('#$%!@&*');
+      $numeros = range(0, 9);
+      $maiusculas = range('A', 'Z');
+      $minusculas = range('a', 'z');
+      $simbolos = str_split('#$%!@&*');
 
-    $senha = '';
+      $senha = '';
 
-    $p_letra = '';
-    $p_letra .= $first == 'numero' ? $numeros[array_rand($numeros)] : '';
-    $p_letra .= $first == 'maiuscula' ? $maiusculas[array_rand($maiusculas)] : '';
-    $p_letra .= $first == 'minuscula' ? $minusculas[array_rand($minusculas)] : '';
-    $p_letra .= $first == 'simbolo' ? $simbolos[array_rand($simbolos)] : '';
+      $p_letra = '';
+      $p_letra .= $first == 'numero' ? $numeros[array_rand($numeros)] : '';
+      $p_letra .= $first == 'maiuscula' ? $maiusculas[array_rand($maiusculas)] : '';
+      $p_letra .= $first == 'minuscula' ? $minusculas[array_rand($minusculas)] : '';
+      $p_letra .= $first == 'simbolo' ? $simbolos[array_rand($simbolos)] : '';
 
-    while (strlen($senha) < $tamanho) {
-      foreach ($seguintes as $key => $value) {
-        $senha .= $value == 'numero' ? $numeros[array_rand($numeros)] : '';
-        $senha .= $value == 'maiuscula' ? $maiusculas[array_rand($maiusculas)] : '';
-        $senha .= $value == 'minuscula' ? $minusculas[array_rand($minusculas)] : '';
-        $senha .= $value == 'simbolo' ? $simbolos[array_rand($simbolos)] : '';
+      while (strlen($senha) < $tamanho) {
+        foreach ($seguintes as $key => $value) {
+          $senha .= $value == 'numero' ? $numeros[array_rand($numeros)] : '';
+          $senha .= $value == 'maiuscula' ? $maiusculas[array_rand($maiusculas)] : '';
+          $senha .= $value == 'minuscula' ? $minusculas[array_rand($minusculas)] : '';
+          $senha .= $value == 'simbolo' ? $simbolos[array_rand($simbolos)] : '';
+        }
       }
+
+      $senha = str_shuffle($senha);
+      $senha = $p_letra . substr($senha, 0, $tamanho - 1);
+
+      echo "<code class=\"round-input\">$senha</code><button class=\"pretty-button\" onclick=\"copyToClipboard('$senha')\">Copiar</button>";
     }
-
-    $senha = str_shuffle($senha);
-    $senha = $p_letra . substr($senha, 0, $tamanho - 1);
-
-    echo "<p>$senha</p><button onclick=\"copyToClipboard('$senha')\">Copiar</button>";
-  }
-  $form = explode("\n", "
-        <form action='senha.php' method='post'>
-        <label for='tamanho'>Tamanho da senha:</label>
-        <input type='number' name='tamanho' id='tamanho' min='8' required" . (isset($tamanho) ? ' value=' . $tamanho : '') . ">
-        <br>
-        <label for='primeiro'>Primeiro caractere:</label>
-        <select name='primeiro' id='primeiro'>
-          <option value='numero'" . (isset($first) && $first === "numero" ? ' selected' : '') . ">Número</option>
-          <option value='maiuscula'" . (isset($first) && $first === "maiuscula" ? ' selected' : '') . ">Maiúscula</option>
-          <option value='minuscula'" . (isset($first) && $first === "minuscula" ? ' selected' : '') . ">Minúscula</option>
-          <option value='simbolo'" . (isset($first) && $first === "simbolo" ? ' selected' : '') . ">Símbolo</option>
-        </select>
-        <br>
-        <label for='seguintes'>Seguintes:</label>
-        <input type='checkbox' name='seguintes[]' id='numero' value='numero'" . (isset($seguintes) && in_array("numero", $seguintes) ? ' checked' : '') . ">
-        <label for='numero'>Número</label>
-        <input type='checkbox' name='seguintes[]' id='maiuscula' value='maiuscula'" . (isset($seguintes) && in_array("maiuscula", $seguintes) ? ' checked' : '') . ">
-        <label for='maiuscula'>Maiúscula</label>
-        <input type='checkbox' name='seguintes[]' id='minuscula' value='minuscula'" . (isset($seguintes) && in_array("minuscula", $seguintes) ? ' checked' : '') . ">
-        <label for='minuscula'>Minúscula</label>
-        <input type='checkbox' name='seguintes[]' id='simbolo' value='simbolo'" . (isset($seguintes) && in_array("simbolo", $seguintes) ? ' checked' : '') . ">
-        <label for='simbolo'>Símbolo</label>
-        <br>
-        <button type='submit'>Enviar</button>
-      </form>");
-  foreach ($form as $index => $line)
-    $form[$index] = str_replace("'", '"', trim($line));
-  echo implode('', $form);
-  ?>
+    $form = explode("\n", "
+          <form action='senha.php' method='post' class'box'>
+          <label for='tamanho'>Tamanho da senha:</label>
+          <input class='round-input' type='number' name='tamanho' id='tamanho' required value='" . (isset($tamanho) ? $tamanho : '8'). '"' . ">
+          <br>
+          <label for='primeiro'>Primeiro caractere:</label>
+          <select class='pretty-button' name='primeiro' id='primeiro'>
+            <option value='numero'" . (isset($first) && $first === "numero" ? ' selected' : '') . ">Número</option>
+            <option value='maiuscula'" . (isset($first) && $first === "maiuscula" ? ' selected' : '') . ">Maiúscula</option>
+            <option value='minuscula'" . (isset($first) && $first === "minuscula" ? ' selected' : '') . ">Minúscula</option>
+            <option value='simbolo'" . (isset($first) && $first === "simbolo" ? ' selected' : '') . ">Símbolo</option>
+          </select>
+          <br>
+          <label for='seguintes'>Seguintes:</label>
+          <input class='round-input' type='checkbox' name='seguintes[]' id='numero' value='numero'" . (isset($seguintes) && in_array("numero", $seguintes) ? ' checked' : '') . ">
+          <label for='numero'>Número</label>
+          <input class='round-input' type='checkbox' name='seguintes[]' id='maiuscula' value='maiuscula'" . (isset($seguintes) && in_array("maiuscula", $seguintes) ? ' checked' : '') . ">
+          <label for='maiuscula'>Maiúscula</label>
+          <input class='round-input' type='checkbox' name='seguintes[]' id='minuscula' value='minuscula'" . (isset($seguintes) && in_array("minuscula", $seguintes) ? ' checked' : '') . ">
+          <label for='minuscula'>Minúscula</label>
+          <input class='round-input' type='checkbox' name='seguintes[]' id='simbolo' value='simbolo'" . (isset($seguintes) && in_array("simbolo", $seguintes) ? ' checked' : '') . ">
+          <label for='simbolo'>Símbolo</label>
+          <br>
+          <button class='pretty-button' type='submit'>Enviar</button>
+        </form>");
+    foreach ($form as $index => $line)
+      $form[$index] = str_replace("'", '"', trim($line));
+    echo implode('', $form);
+    ?>
+  </div>
+  <div class="betinho-container">
+    <h3 class="betinho-title">Confiado por Betinho</h3>
+    <img src="./betinho.png" alt="Betinho" class="betinho">
+  </div>
+  <footer class="bg"></footer>
 </body>
 </html>
