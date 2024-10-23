4ebf90168c847f9c51c38a823e9363bf5271e382
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2024-10-23 12:16:07 -0300

    galeria patch

--- fotografias/index.php
+++ fotografias/index.php
@@ -8,39 +8,44 @@
             onclick="window.location='/fotografias/criar_form.html';"
             value="Cadastrar nova imagem"
         />
+        <style>
+            th:nth-child(2), th:nth-child(3) {
+                width: 75px
+            }
+            tr:nth-child(even) {
+                background-color: #dddddd;
+            }
+        </style>
     </header>
     <main>
         <table style="width: 100%">
-          <tr>
-            <th>Descrição</th>
-            <th>Ver imagem</th>
-            <th>Apagar imagem</th>
-          </tr>
-          <?php
-          include_once "conexao.php";
-          $query = mysqli_query(
-              $conexao,
-              "SELECT * FROM galeria ORDER BY `id` DESC"
-          );
-          $query_array = mysqli_fetch_all($query);
+            <tr>
+                <th>Descrição</th>
+                <th>Ver imagem</th>
+                <th>Apagar imagem</th>
+            </tr>
+            <?php
+            include_once "conexao.php";
+            $query = mysqli_query(
+                $conexao,
+                "SELECT * FROM galeria ORDER BY `id` DESC"
+            );
+            $query_array = mysqli_fetch_all($query);
 
-          // for each index in the array, create a card
-
-          foreach ($query_array as $post) {
-              echo "<tr>";
-              echo "<td>" . $post[1] . "</td>";
-              echo '<td><a href="/fotografias/foto.php?id=' .
-                  $post[0] .
-                  '"><img src="/fotografias/estatico/lupa.png" height="50" width="50"></a></td>';
-              echo '<td><a href="/fotografias/excluir.php?id=' .
-                  $post[0] .
-                  '"><img src="/fotografias/estatico/excluir.png" height="50" width="50"></a></td>';
-              echo "</tr>";
-          }
-          ?>
+            foreach ($query_array as $post) {
+                echo "<tr>";
+                echo "<td>" . $post[1] . "</td>";
+                echo '<td><a href="/fotografias/foto.php?id=' .
+                    $post[0] .
+                    '"><img src="/fotografias/estatico/lupa.png" height="50" width="50"></a></td>';
+                echo '<td><a href="/fotografias/excluir.php?id=' .
+                    $post[0] .
+                    '"><img src="/fotografias/estatico/excluir.png" height="50" width="50"></a></td>';
+                echo "</tr>";
+            }
+            ?>
         </table>
     </main>
-
 </body>
 
 </html>
